e4c12e11010eca7589a5589aec34a99e0acac152
Randy Tolentino <makertolentino@gmail.com>
2015-07-31 05:34:48 -0500

    more practice

---
+++ js/Notes.js
@@ -0,0 +1,15 @@
+var Note = React.createClass ({
+	render: function() {
+		return (
+				<div className="note">
+					<span>
+						<button className="btn btn-primary glyphicon glyphicon-pencil"/>
+						<button className="btn btn-danger glyphicon glyphicon-trash"/>
+					</span>
+				</div>
+			);
+	}
+});
+
+React.render(<Note>Hello World</Note>, 
+	document.getElementById('react-container'));
